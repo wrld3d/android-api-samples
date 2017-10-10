7670703ea50b9368dab7658f10ffb6c7843592b3
Tom Paynter <tom.paynter@dundee.kimbleoperations.com>
2017-10-10 12:40:46 +0100

    Added a temporary activity for developing the PointOnMap changes in.

---
+++ apisamples/src/main/java/com/eegeo/apisamples/TDPActivity.java
@@ -0,0 +1,56 @@
+package com.eegeo.apisamples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.eegeo.mapapi.EegeoApi;
+import com.eegeo.mapapi.EegeoMap;
+import com.eegeo.mapapi.MapView;
+import com.eegeo.mapapi.map.OnMapReadyCallback;
+
+public class TDPActivity extends AppCompatActivity {
+
+    private MapView m_mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialise the EegeoApi with your api key - this needs calling either in the application
+        // instance, or in the activity that contains the MapView
+        EegeoApi.init(this, getString(R.string.eegeo_api_key));
+
+        // The MapView is contained in a layout xml
+        setContentView(R.layout.tdp_activity);
+
+        m_mapView = (MapView) findViewById(R.id.basic_mapview);
+        m_mapView.onCreate(savedInstanceState);
+
+        m_mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(final EegeoMap map) {
+                Toast.makeText(TDPActivity.this, "Welcome to WRLD Maps", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        m_mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        m_mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        m_mapView.onDestroy();
+    }
+
+}
