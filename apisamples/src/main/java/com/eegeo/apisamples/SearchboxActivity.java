46d843a57721f11a51f902e240938e22d8b86314
Michael O'Donnell <michael.odonnell@wrld3d.com>
2017-10-23 09:33:35 +0100

    Create a searchbox example that displays a text area imported from the widgets library.
    Update to Interface names and search background colour.
    
    Changed Search Box Module to use the SearchBoxActivity rather than the base.
    
    Buddy: Michael O
    
    Change to app to use concrete instead of interface - may change based on naming desires.

---
+++ apisamples/src/main/java/com/eegeo/apisamples/SearchboxActivity.java
@@ -0,0 +1,74 @@
+// Copyright eeGeo Ltd (2012-2017), All Rights Reserved
+
+package com.eegeo.apisamples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import com.eegeo.mapapi.EegeoApi;
+import com.eegeo.mapapi.EegeoMap;
+import com.eegeo.mapapi.MapView;
+import com.eegeo.mapapi.map.OnMapReadyCallback;
+import com.wrld.widgets.searchbox.SearchModule;
+
+public class SearchboxActivity extends AppCompatActivity {
+
+    private MapView m_mapView;
+
+    private ViewGroup m_searchView;
+
+    private SearchModule m_searchModule;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (BuildConfig.DEBUG) {
+            // These flags cause the device screen to turn on (and bypass screen guard if possible) when launching.
+            // This makes it easy for developers to test the app launch without needing to turn on the device
+            // each time and without needing to enable the "Stay awake" option.
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                    | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                    | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        }
+
+        // Initialise the EegeoApi with your api key - this needs calling either in the application
+        // instance, or in the activity that contains the MapView
+        EegeoApi.init(this, getString(R.string.eegeo_api_key));
+
+        // The MapView is contained in a layout xml
+        setContentView(R.layout.basic_map_activity);
+
+        m_mapView = (MapView) findViewById(R.id.basic_mapview);
+        m_mapView.onCreate(savedInstanceState);
+
+        m_mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(final EegeoMap map) {
+                ViewGroup uiLayout = (ViewGroup) getLayoutInflater().inflate(R.layout.searchbox_activity_ui, m_mapView, true);
+                m_searchView = (ViewGroup) uiLayout.findViewById(R.id.searchbox_ui);
+                m_searchModule = new SearchModule(m_searchView, SearchboxActivity.this);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        m_mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        m_mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        m_mapView.onDestroy();
+    }
+}
