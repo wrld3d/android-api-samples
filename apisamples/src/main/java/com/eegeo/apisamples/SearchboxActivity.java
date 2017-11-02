793b866d086c93673844ed027278dc1ceef26bc2
SamAins <sam.ainsworth@wrld3d.com>
2017-11-02 11:53:03 +0000

    Added the Suggestion provider to the SeachboxActivity.
    Pair: Michael O

--- apisamples/src/main/java/com/eegeo/apisamples/SearchboxActivity.java
+++ apisamples/src/main/java/com/eegeo/apisamples/SearchboxActivity.java
@@ -12,10 +12,9 @@ import com.eegeo.mapapi.EegeoApi;
 import com.eegeo.mapapi.EegeoMap;
 import com.eegeo.mapapi.MapView;
 import com.eegeo.mapapi.map.OnMapReadyCallback;
-import com.wrld.widgets.searchbox.DebugAutocompleteProvider;
+import com.wrld.widgets.searchbox.DebugSuggestionProvider;
 import com.wrld.widgets.searchbox.DebugSearchProvider;
 import com.wrld.widgets.searchbox.SearchModule;
-import com.wrld.widgets.searchbox.TitleOnlySearchResultViewFactory;
 
 public class SearchboxActivity extends AppCompatActivity {
 
@@ -61,7 +60,7 @@ public class SearchboxActivity extends AppCompatActivity {
                 m_searchModule.setButton((Button)m_searchButtonView.findViewById(R.id.expand_button));
 
                 m_searchModule.addSearchProvider(new DebugSearchProvider("Set 1"));
-                m_searchModule.addAutoCompleteProvider(new DebugAutocompleteProvider("Set 1"));
+                m_searchModule.addSearchProvider(new DebugSuggestionProvider("Set 1"));
                 //m_searchModule.addSearchProvider(new DebugSearchProvider("Set 2"), new TitleOnlySearchResultViewFactory(com.wrld.widgets.R.layout.title_only_search_result));
             }
         });
