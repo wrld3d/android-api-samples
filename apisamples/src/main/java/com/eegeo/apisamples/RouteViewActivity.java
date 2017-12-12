843da78114f437c84d456d85a8c91f71ad221047
Pirh <paul.harris@wrld3d.com>
2017-12-12 16:40:04 +0000

    Squashed commit of the following:
    commit 19032c7649f4e4bddd4b36e9c75d1120190a40e8
    Author: Pirh <paul.harris@wrld3d.com>
    Date:   Tue Dec 12 16:39:32 2017 +0000
    
        Use correct path to RouteView types
    
    commit 3dfd0915136ced0fd4defaacebdbf13829e741e1
    Author: Pirh <paul.harris@wrld3d.com>
    Date:   Mon Dec 11 15:29:52 2017 +0000
    
        Add route styling and route markers examples
    
    commit 072e42b8f39b95d7981c49c1847fcb410e29d2ec
    Author: Pirh <paul.harris@wrld3d.com>
    Date:   Fri Dec 8 16:57:48 2017 +0000
    
        Add RouteView example
    
        Buddy: Jonty
    
    commit 74137dae19bfb60269133a8957fa23d9086a97d3
    Author: Pirh <paul.harris@wrld3d.com>
    Date:   Tue Dec 5 15:15:23 2017 +0000
    
        WIP RouteView

---
+++ apisamples/src/main/java/com/eegeo/apisamples/RouteViewActivity.java
@@ -0,0 +1,104 @@
+package com.eegeo.apisamples;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.eegeo.indoors.IndoorMapView;
+import com.eegeo.mapapi.EegeoApi;
+import com.eegeo.mapapi.EegeoMap;
+import com.eegeo.mapapi.MapView;
+import com.eegeo.mapapi.map.OnMapReadyCallback;
+
+import com.eegeo.mapapi.geometry.LatLng;
+import com.eegeo.mapapi.services.routing.OnRoutingQueryCompletedListener;
+import com.eegeo.mapapi.services.routing.RoutingService;
+import com.eegeo.mapapi.services.routing.RoutingQuery;
+import com.eegeo.mapapi.services.routing.RoutingQueryOptions;
+import com.eegeo.mapapi.services.routing.RoutingQueryResponse;
+import com.eegeo.mapapi.services.routing.Route;
+import com.eegeo.mapapi.widgets.RouteView;
+import com.eegeo.mapapi.widgets.RouteViewOptions;
+
+
+public class RouteViewActivity extends SoftBackButtonActivity implements OnRoutingQueryCompletedListener {
+
+    private MapView m_mapView;
+    private IndoorMapView m_indoorMapView = null;
+    private EegeoMap m_eegeoMap = null;
+    private List<RouteView> m_routeViews = new ArrayList<RouteView>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EegeoApi.init(this, getString(R.string.eegeo_api_key));
+        setContentView(R.layout.routing_indoors_example_activity);
+
+        m_mapView = (MapView) findViewById(R.id.routing_example_mapview);
+        m_mapView.onCreate(savedInstanceState);
+
+        final OnRoutingQueryCompletedListener listener = this;
+
+        m_mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(final EegeoMap map) {
+                m_eegeoMap = map;
+
+                RelativeLayout uiContainer = (RelativeLayout) findViewById(R.id.eegeo_ui_container);
+                m_indoorMapView = new IndoorMapView(m_mapView, uiContainer, m_eegeoMap);
+
+                RoutingService routingService = map.createRoutingService();
+
+                routingService.findRoutes(new RoutingQueryOptions()
+                        .addIndoorWaypoint(new LatLng(56.461231653264029, -2.983122836389253), 2)
+                        .addIndoorWaypoint(new LatLng(56.4600344, -2.9783117), 2)
+                        .onRoutingQueryCompletedListener(listener));
+            }
+        });
+    }
+
+    @Override
+    public void onRoutingQueryCompleted(RoutingQuery query, RoutingQueryResponse response) {
+        Toast.makeText(RouteViewActivity.this, "Found routes", Toast.LENGTH_LONG).show();
+
+        for (Route route: response.getResults()) {
+            RouteViewOptions options = new RouteViewOptions()
+                .color(Color.argb(128, 255, 0, 0))
+                .width(8.0f);
+            RouteView routeView = new RouteView(m_eegeoMap, route, options);
+            m_routeViews.add(routeView);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        m_mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        m_mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (m_eegeoMap != null) {
+            for (RouteView routeView: m_routeViews) {
+                routeView.removeFromMap();
+            }
+        }
+
+        m_mapView.onDestroy();
+    }
+}
+
