c469b1c10edb0c16e35111701a504e6dde98ac98
MichaelTKHChan <michael.chan@kimbleoperations.com>
2018-10-17 11:52:18 +0100

    Squashed commit of the following:
    commit cbfaacda7b5bf03b244c75eaf77b1a2cc8a219a6
    Merge: 66ef6a4 bf3563c
    Author: MichaelTKHChan <michael.chan@kimbleoperations.com>
    Date:   Wed Oct 17 11:50:09 2018 +0100
    
        Merge branch 'master' of https://github.com/wrld3d/android-api-samples into poi-example
    
    commit 66ef6a4fc3fa15ed9fc9d0d5a8205bef873baa78
    Author: MichaelTKHChan <michael.chan@kimbleoperations.com>
    Date:   Tue Oct 9 13:43:00 2018 +0100
    
        Fix for MPLY-10422
        Made the gradle project to open more explicit.
        Buddy: Jordan
    
    commit d47a88ad2599959ec6a7f364ef7af0ac7a7c556a
    Author: MichaelTKHChan <michael.chan@kimbleoperations.com>
    Date:   Tue Oct 9 12:19:33 2018 +0100
    
        Additional fix for MPLY-10423
        Added more requirements for the project.
        Buddy: Jordan
    
    commit 2cccb31190ed19844e3a3f7193520f5538fd32c1
    Author: MichaelTKHChan <michael.chan@kimbleoperations.com>
    Date:   Tue Oct 9 11:58:55 2018 +0100
    
        Fix for MPLY-10424 & 10423
        Buddy: Jordan
    
    commit 8982c5f6a5232d7190a6abaa7b44d92a15a246dc
    Author: MichaelTKHChan <michael.chan@kimbleoperations.com>
    Date:   Thu Oct 4 17:03:21 2018 +0100
    
        Addressing more review comments
        Changes include removing my name from directory path and any other
        files.
        Refactoring POView.java so that showPoiView was smaller.
    
    commit 6d976cf1d04cea80447c2e1bfd26b3cf800c177e
    Author: MichaelTKHChan <michael.chan@kimbleoperations.com>
    Date:   Thu Oct 4 15:15:08 2018 +0100
    
        Review comment changes first pass
        Several fixes made based on review comments.
        Buddy: Jordan
    
    commit b2a6c67fece84c11eef3403761d0d22e1b713e38
    Author: MichaelTKHChan <michael.chan@kimbleoperations.com>
    Date:   Thu Sep 27 12:57:21 2018 +0100
    
        Adding poi tags text functionality
    
    commit c68e8ce426f987efe669f138e772a4e67999b239
    Author: MichaelTKHChan <michael.chan@kimbleoperations.com>
    Date:   Thu Sep 27 12:25:12 2018 +0100
    
        Updating README to be closer to the JS example
    
    commit 9f3933eb9a6612cba21d9eedf452d3250b14fd15
    Author: MichaelTKHChan <michael.chan@kimbleoperations.com>
    Date:   Thu Sep 27 12:05:31 2018 +0100
    
        Adding Android Poi Example project
        Inital commit for the poi example to a branch for review.

---
+++ poi-example/app/src/main/java/com/example/androidpoiexample/TagResources.java
@@ -0,0 +1,21 @@
+// Copyright eeGeo Ltd (2012-2015), All Rights Reserved
+
+package com.example.androidpoiexample;
+
+import android.app.Activity;
+
+public class TagResources
+{
+    public static int getSmallIconForTag(Activity activity, String tag)
+    {
+        return getIconForResourceName(activity, tag);
+    }
+
+    public static int getIconForResourceName(Activity activity, String resourceName)
+    {
+    	String iconPrefix = "icon1_";
+        String uri = "drawable/" + iconPrefix + resourceName;
+        int imageResource = activity.getResources().getIdentifier(uri, null, activity.getPackageName());
+        return imageResource;
+    }
+}
