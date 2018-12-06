70445ce9e838dc265e84034c1e33efbe16539150
johnbell84 <john.bell@eegeo.com>
2018-12-06 09:45:20 +0000

    Props example.  Buddy: Judith

---
+++ apisamples/src/main/java/com/eegeo/apisamples/AddPropIndoorsActivity.java
@@ -0,0 +1,96 @@
+package com.eegeo.apisamples;
+
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.graphics.ColorUtils;
+import android.widget.RelativeLayout;
+
+import com.eegeo.indoors.IndoorMapView;
+import com.eegeo.mapapi.EegeoApi;
+import com.eegeo.mapapi.EegeoMap;
+import com.eegeo.mapapi.MapView;
+import com.eegeo.mapapi.geometry.LatLng;
+import com.eegeo.mapapi.map.OnMapReadyCallback;
+import com.eegeo.mapapi.props.Prop;
+import com.eegeo.mapapi.props.PropOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddPropIndoorsActivity extends WrldExampleActivity {
+
+    private MapView m_mapView;
+    private EegeoMap m_eegeoMap = null;
+    private IndoorMapView m_indoorMapView = null;
+    private List<Prop> m_props = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EegeoApi.init(this, getString(R.string.eegeo_api_key));
+
+        setLockedOrientation();
+
+        setContentView(R.layout.add_prop_indoors_activity);
+        m_mapView = (MapView) findViewById(R.id.add_prop_indoors_mapview);
+        m_mapView.onCreate(savedInstanceState);
+
+        m_mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(final EegeoMap map) {
+                m_eegeoMap = map;
+
+                RelativeLayout uiContainer = (RelativeLayout) findViewById(R.id.eegeo_ui_container);
+                m_indoorMapView = new IndoorMapView(m_mapView, uiContainer, m_eegeoMap);
+
+                final int numberOfFloors = 5;
+                for (int floorId = 0; floorId < numberOfFloors; ++floorId) {
+                    Prop prop = m_eegeoMap.addProp(new PropOptions()
+                            .location(new LatLng(37.782084, -122.404578))
+                            .indoor("intercontinental_hotel_8628", floorId)
+                            .geometryId("duck")
+                            .headingDegrees(90)
+                            .elevation(0.0));
+                    m_props.add(prop);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        m_mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        m_mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (m_eegeoMap != null) {
+            for (Prop prop: m_props) {
+                m_eegeoMap.removeProp(prop);
+            }
+        }
+
+        m_mapView.onDestroy();
+    }
+
+    private void setLockedOrientation() {
+        if (getResources().getBoolean(R.bool.is_large_device)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+        } else {
+            // force portrait on phone
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
+
+}
